b751c6ff82bfc6f9bec357745bdfd95d1b58aca7
GabrielAL-Codex <ga.tc2021@gmail.com>
2021-03-08 18:46:08 -0300

    Tentando alterar para dashboard

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Dashboard from ./Dashboard';
+import Dashboard from './Pages/Dashboard';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
