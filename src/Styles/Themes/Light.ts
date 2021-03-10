e9162775a4bbe5f6a667cd8af0501c5554fe5864
GabrielAL-Codex <ga.tc2021@gmail.com>
2021-03-10 14:45:36 -0300

    coreção

--- src/Styles/Themes/Light.ts
+++ src/Styles/Themes/Light.ts
@@ -3,14 +3,14 @@ export default {
 
     color: {
         primary: '#DCDCDC',
-        secondary: '#FFF',
+        secondary: '#FFFFFF',
         tertiary: '#F5F5F5',
 
         white: '#FFF',
         black: '#000',
         gray: '#BFBFBF',
 
-        success: '#03B85',
+        success: '#03BB85',
         info: '#3B5998',
         warning: '#FF6961'
     }
