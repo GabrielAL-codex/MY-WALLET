1dd3ee999a585993baac3314b8a5d44311b97402
GabrielAL-Codex <ga.tc2021@gmail.com>
2021-03-10 14:44:47 -0300

    Tema light

---
+++ src/Styles/Themes/Light.ts
@@ -0,0 +1,18 @@
+export default {
+    title: 'Light',
+
+    color: {
+        primary: '#DCDCDC',
+        secondary: '#FFF',
+        tertiary: '#F5F5F5',
+
+        white: '#FFF',
+        black: '#000',
+        gray: '#BFBFBF',
+
+        success: '#03B85',
+        info: '#3B5998',
+        warning: '#FF6961'
+    }
+    
+}
