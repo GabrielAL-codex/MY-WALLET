44e13b80d06e149a209ea337923b719fc9fb0530
GabrielAL-Codex <ga.tc2021@gmail.com>
2021-03-10 14:03:44 -0300

    editando arquivo app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import GlobalStyles from './Styles/GlobalStyles';
 
-import Dashboard from './Pages/Dashboard';
+import Layout from './Components/Layout';
 
 const App: React.FC = () => {
     return (
         <>
         <GlobalStyles />
-        <Dashboard />
+        <Layout />
         </>
     );
 }
